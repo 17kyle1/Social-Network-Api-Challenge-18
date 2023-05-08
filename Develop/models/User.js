ac1b153413c9a4151792c18582e73d3cdace34fb
Aaron Turner <Turneraaron97@gmail.com>
2023-05-08 17:19:21 -0400

    Started working on models

---
+++ Develop/models/User.js
@@ -0,0 +1,17 @@
+const { Schema, model} = require('mongoose');
+
+const userSchema = new Schema(
+    {
+        username: { type: String,
+                 required: true,
+                unique: true,
+                 trim: true},
+
+        email: {type: String,
+                required: true,
+                unique: true,
+                match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 
+                'Please fill a valid email address'],
+        }
+    }
+)
