9266e49f1e1eccc8591c5e03f2027a3841c19a5b
Aaron Turner <Turneraaron97@gmail.com>
2023-05-08 20:53:24 -0400

    Continued working on models and index.js

---
+++ Develop/models/Thought.js
@@ -0,0 +1,17 @@
+const {Schema, model} = require('mongoose');
+
+const thoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            minlength: 1,
+            maxlength: 280,
+        },
+        createdAt: {
+           type: Date,
+           default: Date.now(),
+
+        }
+    }
+)
