28fee8796d19be9b3f16aff2d908db018651f764
Aaron Turner <Turneraaron97@gmail.com>
2023-05-15 12:07:06 -0400

    Finished Thought model

--- Develop/models/Thought.js
+++ Develop/models/Thought.js
@@ -1,5 +1,5 @@
 const {Schema, model} = require('mongoose');
-
+const moment = require('moment')
 const thoughtSchema = new Schema(
     {
         thoughtText: {
@@ -11,7 +11,25 @@ const thoughtSchema = new Schema(
         createdAt: {
            type: Date,
            default: Date.now(),
+           get: (currentDate) => moment(currentDate).toDate(),
 
-        }
-    }
-)
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        reactions: [reactionSchema],
+},
+{
+    toJSON: {
+        getters: true
+    },
+}
+);
+thoughtSchema
+.virtual('reactionCount')
+.get(function(){
+    return this.reactions.length
+})
+const Thought = model('Thought', thoughtSchema);
+module.exports = Thought;
